27f915c6a8ce942fbe9b09410ab66aa507476ead
John Spaugh <johnspaugh33@gmail.com>
2026-03-06 18:15:33 -0800

    upload basic names, and project

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# start up page to call on other pages maybe
+#first up is to call on the data set hosted in google drive
+
+import time
+
+def callDataSet():
+    print("starting the fucntin callDataSet for debugging")
+
+
+print("hello World");
